6568d303c3b947e9ab2112785aa526d90f9563dc
SAKSHAM GURBHELE <64558515+sakshamgurbhele@users.noreply.github.com>
2020-12-31 04:50:45 +0530

    Add files via upload

---
+++ typeCasting3/TypeCasting.java
@@ -0,0 +1,26 @@
+package typeCasting3;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int x = 4;
+//		
+//		double y = x;
+//		
+//		System.out.println(x);
+//		System.out.println(y);
+		
+		double myDouble = 2.686876767;
+		
+		int myInt = (int)myDouble;
+		
+		System.out.println(myDouble);
+		
+		System.out.println(myInt);
+				
+		
+      
+	}
+
+}
---
+++ typeCasting3/TypeCastingp1.java
@@ -0,0 +1,23 @@
+package typeCasting3;
+
+public class TypeCastingp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		int myValue = 3;
+//				
+//		long myV = myValue;
+//		
+//		System.out.println(myV);
+		
+		float myG = 3.45f;
+		
+		int MKV = (int)myG;
+		
+		System.out.print(MKV);
+		
+
+	}
+
+}
