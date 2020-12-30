a8744c54e4ebc5b9291930a0988ca1f88d2209b0
SAKSHAM GURBHELE <64558515+sakshamgurbhele@users.noreply.github.com>
2020-12-31 04:50:10 +0530

    Data Types

---
+++ dataTypes2/DataTypres.java
@@ -0,0 +1,15 @@
+package dataTypes2;
+
+public class DataTypres {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   byte x=2 ;
+    System.out.println(x);
+    
+	
+	}
+	
+
+
+}
---
+++ dataTypes2/dataTypesp1.java
@@ -0,0 +1,20 @@
+package dataTypes2;
+
+public class dataTypesp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int age = 14;
+     
+     float rateOfIntrest= 3.24f;
+     
+     double yourRankInJee = 56766567;
+     
+     
+     System.out.println(rateOfIntrest);
+     System.out.println(yourRankInJee);    	
+     System.out.println(age);
+     
+	}
+
+}
